f8f82f9bacccffa5a46792c87f10b14e9a3e41d1
mahiro21 <you@example.com>
2019-11-22 18:11:51 +0100

    :tada: commit inicial

---
+++ src/org/japo/java/app/App.java
@@ -0,0 +1,51 @@
+/* 
+ * Copyright 2019 Mario Merlos Abella - mario.merlos.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.app;
+
+import java.util.Random;
+import org.japo.java.libraries.UtilesPrimitivos;
+
+/**
+ *
+ * @author Mario Merlos Abella - mario.merlos.alum@iescamp.es
+ */
+public final class App {
+
+    //Random
+    private static final Random RND = new Random();
+
+    //Random range
+    public static final int MAX = 10;
+    public static final int MIN = 0;
+
+    //Datos
+    public static final int N1;
+    public static final int N2;
+
+    static {
+        N1 = RND.nextInt(MAX - MIN + 1) + MIN;
+        N2 = RND.nextInt(MAX - MIN + 1) + MIN;
+    }
+
+    public final void launchApp() {
+        System.out.println("OPERACIÓN NUMÉRICA");
+        System.out.println("==================");
+        System.out.printf("Número 1 ....: %d%n", N1);
+        System.out.printf("Número 2 ....: %d%n", N2);
+        System.out.printf(UtilesPrimitivos.operar(N1, N2, 0));
+    }
+
+}
